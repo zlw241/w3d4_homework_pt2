cfe4d43a4104c76175b2926fb1cd7a000ade4fd3
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2017-03-15 22:44:36 -0400

    Fix n+1 queries

---
+++ route.rb
@@ -0,0 +1,36 @@
+class Route < ActiveRecord::Base
+  has_many(
+    :buses,
+    class_name: "Bus",
+    foreign_key: :route_id,
+    primary_key: :id
+  )
+
+  def n_plus_one_drivers
+    buses = self.buses
+
+    all_drivers = {}
+    buses.each do |bus|
+      drivers = []
+      bus.drivers.each do |driver|
+        drivers << driver.name
+      end
+      all_drivers[bus.id] = drivers
+    end
+
+    all_drivers
+  end
+
+  def better_drivers_query
+    # TODO: your code here
+
+    buses = self.buses.includes(:drivers)
+    all_drivers = Hash.new { |h,k| h[k] = [] }
+
+    buses.each do |bus|
+      all_drivers[bus.id] += bus.drivers.select(:name)
+    end
+
+    all_drivers
+  end
+end
